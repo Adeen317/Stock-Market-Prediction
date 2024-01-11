26648bbcb11eb47646d9c2c332401eab68d5dce9
Muhammad Adeen <Adeen317>
2024-01-12 01:51:13 +0500

    Added One Step Prediction

--- LSTM_Stock_Prediction.py
+++ LSTM_Stock_Prediction.py
@@ -58,4 +58,89 @@ train_data = train_data.reshape(-1)
 # Normalize test data
 test_data = scaler.transform(test_data).reshape(-1)
 
+# Now perform exponential moving average smoothing
+# So the data will have a smoother curve than the original ragged data
+EMA = 0.0
+gamma = 0.1
+for ti in range(11000):
+  EMA = gamma*train_data[ti] + (1-gamma)*EMA
+  train_data[ti] = EMA
+
+# Used for visualization and test purposes
+all_mid_data = np.concatenate([train_data,test_data],axis=0)
+
+
+#One-Step Ahead Prediction
+#Standard Averaging
+window_size = 100
+N = train_data.size
+std_avg_predictions = []
+std_avg_x = []
+mse_errors = []
+
+for pred_idx in range(window_size,N):
+
+    if pred_idx >= N:
+        date = dt.datetime.strptime(k, '%Y-%m-%d').date() + dt.timedelta(days=1)
+    else:
+        date = df.loc[pred_idx,'Date']
+
+    std_avg_predictions.append(np.mean(train_data[pred_idx-window_size:pred_idx]))
+    mse_errors.append((std_avg_predictions[-1]-train_data[pred_idx])**2)
+    std_avg_x.append(date)
+
+print('MSE error for standard averaging: %.5f'%(0.5*np.mean(mse_errors)))
+
+
+
+#Exponential Moving Average
+window_size = 100
+N = train_data.size
+
+run_avg_predictions = []
+run_avg_x = []
+
+mse_errors = []
+
+running_mean = 0.0
+run_avg_predictions.append(running_mean)
+
+decay = 0.5
+
+for pred_idx in range(1,N):
+
+    running_mean = running_mean*decay + (1.0-decay)*train_data[pred_idx-1]
+    run_avg_predictions.append(running_mean)
+    mse_errors.append((run_avg_predictions[-1]-train_data[pred_idx])**2)
+    run_avg_x.append(date)
+
+print('MSE error for EMA averaging: %.5f'%(0.5*np.mean(mse_errors)))
+
+#Subplotting
+fig = plt.figure(figsize=(12,6))
+# Add the first subplot (top-left)
+axs1 = fig.add_subplot(1, 2, 1)
+axs1.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
+axs1.plot(range(0,N),run_avg_predictions,color='orange', label='Prediction')
+axs1.set(xlabel = 'Date')
+axs1.set(ylabel = 'Mid Price')
+axs1.set_title("Exponential Moving Average")
+
+# Add the second subplot (top-right)
+axs2 = fig.add_subplot(1, 2, 2)
+axs2.plot(range(df.shape[0]),all_mid_data,color='cyan',label='True')
+axs2.plot(range(window_size,N),std_avg_predictions,color='red',label='Prediction')
+#plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
+axs2.set(xlabel = 'Date')
+axs2.set(ylabel = 'Mid Price')
+axs2.set_title("Standard Averaging")
+
+#Adding a Title for the Overall Figure
+fig.suptitle('One Step Ahead Prediction', fontsize=15)
+
+# Adjust spacing between subplots
+fig.tight_layout()
+
+# Show the figure
+plt.show()
 
