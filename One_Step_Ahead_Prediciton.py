514cf291d3119fb320402d164be3d10e6a22adf7
Muhammad Adeen <Adeen317>
2024-01-17 02:48:36 +0500

    Stock prediction

--- One_Step_Ahead_Prediciton.py
+++ One_Step_Ahead_Prediciton.py
@@ -16,6 +16,15 @@ df = df.sort_values('Date')
 
 # Double check the result
 head=df.head()
+print(head)
+
+#Data Visualization
+plt.figure()
+plt.plot(range(df.shape[0]),(df['Low']+df['High'])/2.0)
+plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500],rotation=45)
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Mid Price',fontsize=18)
+plt.show()
 
 # First calculate the mid prices from the highest and lowest
 high_prices = df.loc[:,'High'].to_numpy()
@@ -84,60 +93,54 @@ print('MSE error for standard averaging: %.5f'%(0.5*np.mean(mse_errors)))
 
 
 
+#Exponential Moving Average
+window_size = 100
+N = train_data.size
 
+run_avg_predictions = []
+run_avg_x = []
 
-class DataGeneratorSeq(object):
+mse_errors = []
 
-    def __init__(self,prices,batch_size,num_unroll):
-        self._prices = prices
-        self._prices_length = len(self._prices) - num_unroll
-        self._batch_size = batch_size
-        self._num_unroll = num_unroll
-        self._segments = self._prices_length //self._batch_size
-        self._cursor = [offset * self._segments for offset in range(self._batch_size)]
+running_mean = 0.0
+run_avg_predictions.append(running_mean)
 
-    def next_batch(self):
+decay = 0.5
 
-        batch_data = np.zeros((self._batch_size),dtype=np.float32)
-        batch_labels = np.zeros((self._batch_size),dtype=np.float32)
+for pred_idx in range(1,N):
 
-        for b in range(self._batch_size):
-            if self._cursor[b]+1>=self._prices_length:
-                #self._cursor[b] = b * self._segments
-                self._cursor[b] = np.random.randint(0,(b+1)*self._segments)
+    running_mean = running_mean*decay + (1.0-decay)*train_data[pred_idx-1]
+    run_avg_predictions.append(running_mean)
+    mse_errors.append((run_avg_predictions[-1]-train_data[pred_idx])**2)
+    run_avg_x.append(date)
 
-            batch_data[b] = self._prices[self._cursor[b]]
-            batch_labels[b]= self._prices[self._cursor[b]+np.random.randint(0,5)]
+print('MSE error for EMA averaging: %.5f'%(0.5*np.mean(mse_errors)))
 
-            self._cursor[b] = (self._cursor[b]+1)%self._prices_length
+#Subplotting
+fig = plt.figure(figsize=(12,6))
+# Add the first subplot (top-left)
+axs1 = fig.add_subplot(1, 2, 1)
+axs1.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
+axs1.plot(range(0,N),run_avg_predictions,color='orange', label='Prediction')
+axs1.set(xlabel = 'Date')
+axs1.set(ylabel = 'Mid Price')
+axs1.set_title("Exponential Moving Average")
 
-        return batch_data,batch_labels
+# Add the second subplot (top-right)
+axs2 = fig.add_subplot(1, 2, 2)
+axs2.plot(range(df.shape[0]),all_mid_data,color='cyan',label='True')
+axs2.plot(range(window_size,N),std_avg_predictions,color='red',label='Prediction')
+#plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
+axs2.set(xlabel = 'Date')
+axs2.set(ylabel = 'Mid Price')
+axs2.set_title("Standard Averaging")
 
-    def unroll_batches(self):
+#Adding a Title for the Overall Figure
+fig.suptitle('One Step Ahead Prediction', fontsize=15)
 
-        unroll_data,unroll_labels = [],[]
-        init_data, init_label = None,None
-        for ui in range(self._num_unroll):
+# Adjust spacing between subplots
+fig.tight_layout()
 
-            data, labels = self.next_batch()    
+# Show the figure
+plt.show()
 
-            unroll_data.append(data)
-            unroll_labels.append(labels)
-
-        return unroll_data, unroll_labels
-
-    def reset_indices(self):
-        for b in range(self._batch_size):
-            self._cursor[b] = np.random.randint(0,min((b+1)*self._segments,self._prices_length-1))
-
-
-
-dg = DataGeneratorSeq(train_data,5,5)
-u_data, u_labels = dg.unroll_batches()
-
-for ui,(dat,lbl) in enumerate(zip(u_data,u_labels)):   
-    print('\n\nUnrolled index %d'%ui)
-    dat_ind = dat
-    lbl_ind = lbl
-    print('\tInputs: ',dat )
-    print('\n\tOutput:',lbl)
