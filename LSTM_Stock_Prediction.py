b0e937b2e372fcff05427f883cd6b82b9229be0f
Muhammad Adeen <Adeen317>
2024-01-21 02:21:35 +0500

    LSTM prediction

--- LSTM_Stock_Prediction.py
+++ LSTM_Stock_Prediction.py
@@ -164,18 +164,42 @@ for ui in range(num_unrollings):
 #Regression Layer
 
 lstm_cells = [
-    tf.contrib.rnn.LSTMCell(num_units=num_nodes[li],
+    tf.compat.v1.estimator.rnn.LSTMCell(num_units=num_nodes[li],
                             state_is_tuple=True,
                             initializer= tf.contrib.layers.xavier_initializer()
                            )
  for li in range(n_layers)]
 
-drop_lstm_cells = [tf.contrib.rnn.DropoutWrapper(
+drop_lstm_cells = [tf.compat.v1.estimator.rnn.DropoutWrapper(
     lstm, input_keep_prob=1.0,output_keep_prob=1.0-dropout, state_keep_prob=1.0-dropout
 ) for lstm in lstm_cells]
-drop_multi_cell = tf.contrib.rnn.MultiRNNCell(drop_lstm_cells)
-multi_cell = tf.contrib.rnn.MultiRNNCell(lstm_cells)
+drop_multi_cell = tf.compat.v1.estimator.rnn.MultiRNNCell(drop_lstm_cells)
+multi_cell = tf.compat.v1.estimator.rnn.MultiRNNCell(lstm_cells)
 
+
+#LSTM output
 w = tf.get_variable('w',shape=[num_nodes[-1], 1], initializer=tf.contrib.layers.xavier_initializer())
 b = tf.get_variable('b',initializer=tf.random_uniform([1],-0.1,0.1))
 
+# Create cell state and hidden state variables to maintain the state of the LSTM
+c, h = [],[]
+initial_state = []
+for li in range(n_layers):
+  c.append(tf.Variable(tf.zeros([batch_size, num_nodes[li]]), trainable=False))
+  h.append(tf.Variable(tf.zeros([batch_size, num_nodes[li]]), trainable=False))
+  initial_state.append(tf.contrib.rnn.LSTMStateTuple(c[li], h[li]))
+
+# Do several tensor transofmations, because the function dynamic_rnn requires the output to be of
+# a specific format. Read more at: https://www.tensorflow.org/api_docs/python/tf/nn/dynamic_rnn
+all_inputs = tf.concat([tf.expand_dims(t,0) for t in train_inputs],axis=0)
+
+# all_outputs is [seq_length, batch_size, num_nodes]
+all_lstm_outputs, state = tf.nn.dynamic_rnn(
+    drop_multi_cell, all_inputs, initial_state=tuple(initial_state),
+    time_major = True, dtype=tf.float32)
+
+all_lstm_outputs = tf.reshape(all_lstm_outputs, [batch_size*num_unrollings,num_nodes[-1]])
+
+all_outputs = tf.nn.xw_plus_b(all_lstm_outputs,w,b)
+
+split_outputs = tf.split(all_outputs,num_unrollings,axis=0)
