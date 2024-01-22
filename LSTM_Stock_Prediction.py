c6959af2317b9cf91c949bc926a072455569d58f
Muhammad Adeen <Adeen317>
2024-01-22 19:56:50 +0500

    LSTM Stock Market Prediction

--- LSTM_Stock_Prediction.py
+++ LSTM_Stock_Prediction.py
@@ -203,3 +203,35 @@ all_lstm_outputs = tf.reshape(all_lstm_outputs, [batch_size*num_unrollings,num_n
 all_outputs = tf.nn.xw_plus_b(all_lstm_outputs,w,b)
 
 split_outputs = tf.split(all_outputs,num_unrollings,axis=0)
+
+# When calculating the loss you need to be careful about the exact form, because you calculate
+# loss of all the unrolled steps at the same time
+# Therefore, take the mean error or each batch and get the sum of that over all the unrolled steps
+
+print('Defining training Loss')
+loss = 0.0
+with tf.control_dependencies([tf.assign(c[li], state[li][0]) for li in range(n_layers)]+
+                             [tf.assign(h[li], state[li][1]) for li in range(n_layers)]):
+  for ui in range(num_unrollings):
+    loss += tf.reduce_mean(0.5*(split_outputs[ui]-train_outputs[ui])**2)
+
+print('Learning rate decay operations')
+global_step = tf.Variable(0, trainable=False)
+inc_gstep = tf.assign(global_step,global_step + 1)
+tf_learning_rate = tf.placeholder(shape=None,dtype=tf.float32)
+tf_min_learning_rate = tf.placeholder(shape=None,dtype=tf.float32)
+
+learning_rate = tf.maximum(
+    tf.train.exponential_decay(tf_learning_rate, global_step, decay_steps=1, decay_rate=0.5, staircase=True),
+    tf_min_learning_rate)
+
+# Optimizer.
+print('TF Optimization operations')
+optimizer = tf.train.AdamOptimizer(learning_rate)
+gradients, v = zip(*optimizer.compute_gradients(loss))
+gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
+optimizer = optimizer.apply_gradients(
+    zip(gradients, v))
+
+print('\tAll done')
+
